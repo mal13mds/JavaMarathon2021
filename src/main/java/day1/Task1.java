cbc42e703ec18cd363fc1bbe250e70e5d4e4fbe2
mal13mds <mal_mds@mail.ru>
2021-01-30 20:04:49 +0300

    Add Task1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+   int a = 0;
+   while (a < 10){
+            System.out.print("JAVA" + " ");
+            a = a +1 ;
+        }
     }
 }
